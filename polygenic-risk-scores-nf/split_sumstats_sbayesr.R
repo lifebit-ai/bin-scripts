808f753419d43a84db6af486f4f870e3e88da50c
Konrad Zych <konrad@lifebit.ai>
2022-10-20 16:38:29 +0100

    dev

--- polygenic-risk-scores-nf/split_sumstats_sbayesr.R
+++ polygenic-risk-scores-nf/split_sumstats_sbayesr.R
@@ -1,4 +1,4 @@
-#/usr/bin/env Rscript
+#!/usr/bin/env Rscript
 
 ################################################################################
 # Script for splitting input GWAS summary statistics by chromosome
