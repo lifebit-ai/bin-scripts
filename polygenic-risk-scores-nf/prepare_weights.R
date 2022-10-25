789054229b4f6dd8e662f5c0e4c98eb4d555c58b
Konrad Zych <konrad@lifebit.ai>
2022-10-25 13:23:22 +0100

    dev

--- polygenic-risk-scores-nf/prepare_weights.R
+++ polygenic-risk-scores-nf/prepare_weights.R
@@ -1,4 +1,4 @@
-#/usr/bin/env Rscript
+#!/usr/bin/env Rscript
 
 ################################################################################
 # Script for reformatting MEGAPRS output variant weights into a format compliant
